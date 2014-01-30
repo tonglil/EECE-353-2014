baf3ca1b23edb68773ab71bd0cd549a15d898fc7
Tony Li <tonyli.lives@gmail.com>
2014-01-30 15:11:50 -0800

    lab2 hand in

--- Lab2/Done/Demo1/lab2.vhd
+++ Lab2/Done/Demo1/lab2.vhd
@@ -1,3 +1,6 @@
+-- Your Names:  Amitoj Kooner, Tongli Li
+-- Your Student Numbers: 33263112, 15688112
+-- Your Lab Section:  L2C
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -5,17 +8,17 @@ use ieee.std_logic_unsigned.all;
 
 entity lab2 is
 	port(key : in std_logic_vector(0 downto 0);  -- pushbutton switches
-            sw : in std_logic_vector(8 downto 0);  -- slide switches
-            lcd_rw : out std_logic;
-            lcd_en : out std_logic;
-            lcd_rs : out std_logic;
-            lcd_on : out std_logic;
-            lcd_blon : out std_logic;
-            lcd_data : out std_logic_vector(7 downto 0);
-				ledg : out std_logic_vector(5 downto 0);
-				CLOCK_50 : in std_logic );  -- one of the 7-segment diplays
-end lab2 ;
-
+		sw : in std_logic_vector(8 downto 0);  -- slide switches
+		lcd_rw : out std_logic;
+		lcd_en : out std_logic;
+		lcd_rs : out std_logic;
+		lcd_on : out std_logic;
+		lcd_blon : out std_logic;
+		lcd_data : out std_logic_vector(7 downto 0);
+		ledg : out std_logic_vector(5 downto 0);
+		CLOCK_50 : in std_logic
+	);  -- one of the 7-segment diplays
+end lab2;
 
 architecture behavioural of lab2 is
 	TYPE state_types is (r0, r1, r2, r3, r4, r5, c1, c2, c3, c4, c5, m0, m1, m2);
@@ -142,16 +145,16 @@ begin
 				lcd_data <= "01000001"; --write A
 			WHEN c2 =>
 				lcd_rs <= '1';
-				lcd_data <= "01001101"; --write M
+				lcd_data <= "01101101"; --write M
 			WHEN c3 =>
 				lcd_rs <= '1';
-				lcd_data <= "01001001"; --write I
+				lcd_data <= "01101001"; --write I
 			WHEN c4 =>
 				lcd_rs <= '1';
-				lcd_data <= "01010100"; --write T
+				lcd_data <= "01110100"; --write T
 			WHEN c5 =>
 				lcd_rs <= '1';
-				lcd_data <= "01001111"; --write O
+				lcd_data <= "01101111"; --write O
 			WHEN m0 =>
 				lcd_rs <= '0';
 				lcd_data <= "11000000"; --move cursor to second row
