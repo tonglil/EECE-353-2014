85a3d6522fcad9e619fc1ce69c10d799be058e50
Tony Li <tonyli.lives@gmail.com>
2014-02-13 12:34:18 -0800

    datapath

--- Lab3/Skeleton Files/datapath.vhd
+++ Lab3/Skeleton Files/datapath.vhd
@@ -30,45 +30,171 @@ END datapath;
 
 ARCHITECTURE mixed OF datapath IS
 
+	COMPONENT dealcard
+		PORT(
+			clock : IN  STD_LOGIC;
+			resetb : IN  STD_LOGIC;
+			new_card  : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)	-- new card to deal
+		);
+	END COMPONENT;
+
+	COMPONENT reg4
+		PORT(	
+			I:	in std_logic_vector(3 downto 0);
+			slow_clock:	in std_logic;
+			load:	in std_logic;
+			resetb:	in std_logic;
+			Q:	out std_logic_vector(3 downto 0)
+		);
+	END COMPONENT;
+
+	COMPONENT card7seg
+		PORT(
+			card : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);  -- card type (Ace, 2..10, J, Q, K)
+			seg7 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)   -- top seg 'a' = bit0, proceed clockwise
+		);
+	END COMPONENT;
+
+	COMPONENT scorehand
+		PORT(
+			card1, card2, card3 : IN STD_LOGIC_VECTOR(3 downto 0);
+			total : OUT STD_LOGIC_VECTOR( 3 DOWNTO 0)  -- total value of hand
+		);
+	END COMPONENT;
+
+	COMPONENT score7seg
+		PORT(
+			score : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);  -- score (0 to 9)
+			seg7 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)   -- top seg 'a' = bit0, proceed clockwise
+		);
+	END COMPONENT;
+
+	SIGNAL new_card : STD_LOGIC_VECTOR(3 downto 0);			-- new_card
+	SIGNAL pcard1_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary player card
+	SIGNAL pcard2_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary player card
+	SIGNAL pcard3_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary player card
+	SIGNAL pscore : STD_LOGIC_VECTOR(3 downto 0);			-- temporary player score
+	SIGNAL dcard1_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary dealer card
+	SIGNAL dcard2_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary dealer card
+	SIGNAL dcard3_sig : STD_LOGIC_VECTOR(3 downto 0);		-- temporary dealer card
+	SIGNAL dscore : STD_LOGIC_VECTOR(3 downto 0);			-- temporary dealer score
+
 BEGIN
-	PROCESS(slow_clock, resetb, load_pcard1, load_pcard2, load_pcard3, load_dcard1, load_dcard2, load_dcard3)
-	BEGIN
-		IF rising_edge(slow_clock) THEN
-			-- if enable (load_pcard1) is high -> dealcard => register
-			IF load_pcard1 = '1' THEN
-			END IF;
-		END IF;
-    -- Your code goes here
-	 END PROCESS;
-	 
-	 
-	 
-	 
+
+	dc : dealcard PORT MAP (
+		clock => fast_clock,
+		resetb => resetb,
+		new_card => new_card
+	);
+	
+	-- Player datapaths
+	pcard1 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_pcard1,
+		resetb => resetb,
+		Q => pcard1_sig
+	);
+
+	pcard2 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_pcard2,
+		resetb => resetb,
+		Q => pcard2_sig
+	);
+
+	pcard3 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_pcard3,
+		resetb => resetb,
+		Q => pcard3_sig
+	);
+	
+	pcard3_out <= pcard3_sig;
+	
+	pcard1seg : card7seg PORT MAP(
+		card : pcard1_sig;
+		seg7 : HEX0
+	);
+	
+	pcard2seg : card7seg PORT MAP(
+		card : pcard2_sig;
+		seg7 : HEX1
+	);
+	
+	pcard3seg : card7seg PORT MAP(
+		card : pcard3_sig;
+		seg7 : HEX2
+	);
+	
+	pscorehand : scorehand PORT MAP(
+		card1 => pcard1_sig,
+		card2 => pcard2_sig,
+		card3 => pcard3_sig,
+		total => pscore
+	);
+	
+	pscore_out <= pscore;
+
+	p7seg : score7seg PORT Map(
+		score => pscore,
+		seg7 => HEX3
+	);
+
+	-- Dealer datapaths
+	dcard1 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_dcard1,
+		resetb => resetb,
+		Q => dcard1_sig
+	);
+
+	dcard2 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_dcard2,
+		resetb => resetb,
+		Q => dcard2_sig
+	);
+
+	dcard3 : reg4 PORT MAP(	
+		I => new_card,
+		slow_clock => slow_clock,
+		load => load_dcard3,
+		resetb => resetb,
+		Q => dcard3_sig
+	);
+
+	dcard1seg : card7seg PORT MAP(
+		card : dcard1_sig;
+		seg7 : HEX4
+	);
+	
+	dcard2seg : card7seg PORT MAP(
+		card : dcard2_sig;
+		seg7 : HEX5
+	);
+	
+	dcard3seg : card7seg PORT MAP(
+		card : dcard3_sig;
+		seg7 : HEX6
+	);
+	
+	dscorehand : scorehand PORT MAP(
+		card1 => dcard1_sig,
+		card2 => dcard2_sig,
+		card3 => dcard3_sig,
+		total => dscore
+	);
+	
+	dscore_out <= dscore;
+	
+	d7seg : score7seg PORT Map(
+		score => dscore,
+		seg7 => HEX7
+	);
+	
 END;
-
-
--- add reg4 here
-
------
-
---ARCHITECTURE behavioral OF dealcard IS
---	SIGNAL	dealer_card : UNSIGNED(3 DOWNTO 0);
---	
---BEGIN
---	-- The dealer is always shuffling the deck
---	PROCESS( clock, resetb )
---	BEGIN
---		IF resetb='0' THEN
---			dealer_card <= "0001";
---		ELSIF RISING_EDGE(clock) THEN
---			IF dealer_card = "1101" THEN
---				dealer_card <= "0001";
---			ELSE
---				dealer_card <= dealer_card + 1;
---			END IF;
---		END IF;
---	END PROCESS;
---
---	new_card <= std_logic_vector(dealer_card);
---END;
---
--- Lab3/Skeleton Files/reg4.vhd
+++ Lab3/Skeleton Files/reg4.vhd
@@ -5,35 +5,31 @@ use ieee.std_logic_unsigned.all;
 
 ENTITY reg4 IS
 	PORT(	
-		I:	in std_logic_vector(4 downto 0);
+		I:	in std_logic_vector(3 downto 0);
 		slow_clock:	in std_logic;
 		load:	in std_logic;
-		resetb:	in std_logic;
-		Q:	out std_logic_vector(4 downto 0)
+		resetb: in std_logic;
+		Q:	out std_logic_vector(3 downto 0)
 );
 END reg4;
 
 
 ARCHITECTURE behavioral OF reg4 IS
 
-signal Q_tmp: std_logic_vector(4 downto 0);
+signal Q_tmp: std_logic_vector(3 downto 0);
 
 BEGIN
 	process(I, slow_clock, load, resetb)
 	begin
-
 		if resetb = '0' then
 			-- use 'range in signal assigment 
 			Q_tmp <= (Q_tmp'range => '0');
-		elsif (slow_clock='1' and clock'event) then
+		elsif (slow_clock='1' and slow_clock'event) then
 			if load = '1' then
 				Q_tmp <= I;
 			end if;
 		end if;
-
 	end process;
-
 	-- concurrent statement
 	Q <= Q_tmp;
-	 
 END;
